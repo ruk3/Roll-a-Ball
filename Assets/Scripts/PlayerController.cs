c6f00a2edab9980de4365268b42419422c3c4c8b
ruk3 <61035764+ruk3@users.noreply.github.com>
2021-04-24 18:33:44 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 jumpForce = new Vector3(0.0f, 200.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown("space") && isGrounded)
+        {
+            rb.AddForce(jumpForce);
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Roll-a-Ball
+ Roll a Ball game from Unity Learn and assignment 1 for Character Animation
